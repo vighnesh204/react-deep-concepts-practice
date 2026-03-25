dd9adacfeaa9295adcce1f623c900c346d5b231c
vighnesh204 <borhadevighnesh@gmail.com>
2026-03-25 22:06:53 +0530

    setup redux store and provide it to the application from main.jsx

--- Namaste-Food/src/main.jsx
+++ Namaste-Food/src/main.jsx
@@ -1,12 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { RouterProvider } from "react-router-dom"
-import { appRouter } from './App.jsx'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { appRouter } from "./App.jsx";
+import { Provider } from "react-redux"
+import "./index.css";
+import App from "./App.jsx";
+import appStore from "./utils/appStore.js";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+
+createRoot(document.getElementById("root")).render(
+  <Provider store={appStore}>
     <RouterProvider router={appRouter} />
-  </StrictMode>,
-)
+  </Provider>,
+);
