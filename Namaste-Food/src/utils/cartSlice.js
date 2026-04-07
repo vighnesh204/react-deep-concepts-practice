7d338054148e6bc8a9e9bcd9ab34407a7729bebe
vighnesh204 <borhadevighnesh@gmail.com>
2026-04-07 14:58:50 +0530

    cartSlice creatd

--- Namaste-Food/src/utils/cartSlice.js
+++ Namaste-Food/src/utils/cartSlice.js
@@ -1,3 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const cartSlice = createSlice()
+const cartSlice = createSlice({
+    name: "cart",
+    initialState: {
+        items: []
+    },
+    reducers: {
+        addItem: (state, action) => {
+            // mutating the state here
+            state.items.push(action.payload);
+        },
+        removeItem: (state, action) => {
+            state.items = state.items.filter(item => item.id !== action.payload);
+        },
+        clearCart: (state) => {
+            state.items = [];
+        }
+    }
+})
+
+export const { addItem, removeItem, clearCart } = cartSlice.actions;
+export default cartSlice.reducer;
